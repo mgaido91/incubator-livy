308823bb3adb5e7397355b50cd5a3a0436258642
Marco Gaido <mgaido@hortonworks.com>
2018-07-02 17:29:45 +0200

    use thrift 0.9.3

---
+++ thriftserver/src/main/scala/org/apache/livy/thriftserver/LivyThriftserver.scala
@@ -0,0 +1,16 @@
+package org.apache.livy.thriftserver
+
+import org.apache.livy.Logging
+
+/**
+ * The main entry point for the Livy thrift server leveraging HiveServer2. Starts up a
+ * `HiveThriftServer2` thrift server.
+ */
+object LivyThriftserver extends Logging {
+
+}
+
+
+class LivyThriftserver {
+
+}
